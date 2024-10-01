b818ad1dc2732ead7774e708259f86cfee1ff6b3
megamaharani14 <megamaharani661@gmail.com>
2024-10-02 03:10:10 +0700

    add images

--- flutter_application_1/README.md
+++ flutter_application_1/README.md
@@ -10,8 +10,17 @@ Langkah 2: buka main.dart lalu ganti kode lalu isi nama dan nim pada text title.
 ![Screenshot flutter_application_1](images/layout.png)
 
 Langkah 3: identifikasi layout diagram
+1, identifikasi elemen yang lebih besar
+2, buat diagram setiap baris
+3, setelah tata letak dibuat lalu pendekatan bottom-up
+
 Langkah 4: implementasi title row 
+Widget ini akan menampilkan judul lokasi wisata dan deskripsi serta ikon bintang.
+![Screenshot flutter_application_1](images/title.png)
+
 
 
 Praktikum 2: Implementasi button row 
-Langkah 1: buat method column_buildButtonColumn 
+Langkah 1: buat method column_buildButtonColumn
+
+
--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,22 +1,86 @@
 import 'package:flutter/material.dart';
-void main() => runApp(const MyApp());
+
+void main() {
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-   title: 'Flutter layout: Mega Maharani dan 362358302010',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter layout demo'),
-        ),
-        body: const Center(
-          child: Text('Mega Maharani dan 362358302010'),
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32), 
+      child: Row(
+        children: [
+          Expanded(
+            // soal 1: Letakkan widget Column di dalam widget Expanded
+            child: Column(
+              // soal 1: Tambahkan properti crossAxisAlignment
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // soal 2: Letakkan baris pertama teks di dalam container dan menambahkan padding = 8
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Wisata Gunung di Batu',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Batu, Malang, Indonesia',
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
+          ),
+          // soal 3: Tambahkan ikon dan teks angka
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+        ],
+      ),
+    );
+
+    return Scaffold(
+    appBar: AppBar(
+      title: const Text('Icon Columns Example'),
+    ),
+    body: Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(Colors.blue, Icons.call, 'CALL'),
+          _buildButtonColumn(Colors.blue, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(Colors.blue, Icons.share, 'SHARE'),
+        ],
         ),
       ),
     );
   }
 }
+
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
